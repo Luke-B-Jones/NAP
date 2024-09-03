2ccac206856d89cd205d47df71279b2ec02d9781
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2024-09-03 14:35:59 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+echo "# Setting up directorys (1 of 3)"
 source config.sh
-
-# Save directory and overwrite config
-echo "(${su}Step ${B}1 | 5${r}${su}:${in} Capturing directories and setting-up config{r})"
+if [ ! -f config.sh ]; then
+    echo "ERROR: config.sh not found!"
+    exit 1
+fi
 # Capture NAP directory and version
-loc=$(pwd)
+loc="$1"
+# Build file structure
+mkdir -p "${loc}/bin/"
+mv "${loc}/scripts/" "${loc}/bin/"
+mv "${loc}/subconfigs/" "${loc}/bin/"
+mkdir -p "${loc}/bin/databases"
+mkdir -p "${loc}/bin/logs"
+ln -s "${loc}/bin/scripts/nap.sh" "${loc}/nap"
+#
+echo "# Populating the config (2 of 3)"
 # Update config via variable terminology
 update_config() {
     local var_name=$1
@@ -23,34 +34,12 @@ update_config "pipeline_version" "$NAP_version"
 update_config "all_cores" "$thread_count"
 update_config "all_RAM" "$ram_bytes"
 update_config "w_d" "$loc"
-# Build file structure
-echo "(${su}Step ${B}2 | 5${r}${su}:${in} Generating file structure ${r})"
-mkdir -p "${loc}/bin/databases"
-mkdir -p "${loc}/bin/scripts"
-mkdir -p "${loc}/bin/subconfig"
-mkdir -p "${loc}/bin/logs"
-
-# Move git clone files into correct locations
-mv "${loc}/*.py" "${loc}/bin/scripts"
-mv "${loc}/AMP_515y-926r.sh" "${loc}/bin/subconfig"
-mv "${loc}/hardware-heavy.sh" "${loc}/bin/subconfig"
-mv "${loc}/hardware-light.sh" "${loc}/bin/subconfig"
-mv "${loc}/hardware-super-light.sh" "${loc}/bin/subconfig"
-sleep 2
-mv "${loc}/*.sh" "${loc}/bin/scripts"
-
-# NAP wrapper setup
-ln -s "${loc}/bin/scripts/nap.sh" "${loc}/nap"
-# Grant chmod +x all scripts
-echo "(${su}Step ${B}3 | 5${r}${su}:${in} Modifying script permissions{r})"
-chmod -R +x "${loc}/*"
-
+#
 # Setup database
+echo "# Generating a SILVA database (3 of 3)"
 cd "${loc}/bin/databases/"
-wget -O SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz "https://www.arb-silva.de/no_cache/download/archive/release_138_2/Exports/SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz"
-gzip -d SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz
-SILVA_prefix="SILVA_138.2_SSURef_NR99_tax_silva"
-echo "(${su}Step ${B}4 | 5${r}${su}:${in} Generating databases${r})"
+wget -O SILVA_138.2_SSU_NR99.fasta.gz "https://www.arb-silva.de/fileadmin/silva_databases/release_138_2/Exports/SILVA_138.2_SSURef_NR99_tax_silva.fasta.gz" || { echo "ERROR: Failed to download SILVA database"; exit 1; }
+gzip -d SILVA_138.2_SSU_NR99.fasta.gz
+SILVA_prefix="SILVA_138.2_SSU_NR99"
 # Create databases in manual (0)
 bash "${loc}/bin/scripts/update-database.sh" "$SILVA_prefix" "0"
-echo "(${su}Step ${B}5 | 5${r}${su}:${in} Pipeline setup complete${r})"
