9a5737efc36c02d442acc6586754ed19dfc9f33a
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2024-09-04 11:11:17 +0100

    AMP_template.sh

---
+++ subconfigs/AMP_template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Filtering
+export max_length=""
+export min_length=""
+# Blastn CON
+export ident_CON_16s="95"
+export ident_CON_18s="95"
+# Blastn RAW
+export ident_RAW_16s="80"
+export ident_RAW_18s="80"
+# Bias correction
+export bias_factor_16s=""
+export bias_factor_18s=""
+# Database info
+export for_seq=""
+export rev_seq=""
+export for_range=""
+export rev_range=""
