17cc3dc5bc212165018ec0004a2985e73ea0aa61
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2024-09-09 16:54:03 +0100

    AMP_515y-926r.sh

--- subconfigs/AMP_515y-926r.sh
+++ subconfigs/AMP_515y-926r.sh
@@ -3,8 +3,8 @@
 export max_length="1200"
 export min_length="300"
 # Blastn CON
-export ident_CON_16s="95"
-export ident_CON_18s="95"
+export ident_CON_16s="92"
+export ident_CON_18s="92"
 # Blastn RAW
 export ident_RAW_16s="80"
 export ident_RAW_18s="80"
