83007882c03769b5603a76903932d06855d01325
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2024-09-09 13:58:49 +0100

    pipe.sh

--- scripts/pipe.sh
+++ scripts/pipe.sh
@@ -49,7 +49,7 @@ fi
 # Setup amplicon configuration based on amplicon_pre_set
 source "${subconfig}/AMP_${amplicon_pre_set}.sh"
 # Check database is setup corretly
-required_files=("fasta_18s_database" "fasta_16s_database" "blastn_16s_database" "blastn_18s_database" "fasta_filtered_database")
+required_files=("fasta_18s_database" "fasta_16s_database" "fasta_filtered_database")
 for file_var in "${required_files[@]}"; do
     file_path="${!file_var}"  # Get the actual file path from the variable name
     if [ ! -s "$file_path" ]; then
