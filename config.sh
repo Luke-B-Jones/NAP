e312cd9c33fb95bfa30c420e4790f0735262709f
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2024-09-09 13:48:35 +0100

    config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 # PC settings
-export hardware_use="heavy" # 'super-light' uses 20%, 'light' uses 45%, and 'heavy' will use 95% of your computational resourses
-export amplicon_pre_set="515y-926r" # premade configs to change amplicon handeling
-# Dorado AUTO settings
-export defualt_kit="SQK-NBD114-24" # Dorado
-export defualt_model="dna_r10.4.1_e8.2_400bps_sup@v5.0.0" # Dorado
-export demux_needed="yes" # yes or no do you need demux step in dorado
+export hardware_use="heavy"         # 'super-light' uses 20%, 'light' uses 45%, and 'heavy' will use 95% of your computational resourses
+export amplicon_pre_set="515y-926r" # premade configs to change amplicon handeling 
 # Defualt QC info (pipe)
 export norm_factor="100000" # Normalisation factor
-export max_depth="230000" # Recommend 'true max' + 20%
+export max_depth="230000"   # Recommend 'true max' + 20%
 # Phred scaling
 export phred_500k="30" # Phred if input is >0.5 million reads
 export phred_300k="28" # Phred if input is >0.3 million reads
@@ -45,7 +41,7 @@ export reduce_to_abundance="${w_d}/bin/scripts/reduce_to_abundance.py"       # R
 export blast_consensus="${w_d}/bin/scripts/blast_consensus.py"               # CON hit consensus
 export generate_con_database="${w_d}/bin/scripts/generate_con_database.py"   # Generate database from CON blastn hits (for raw data)
 export remove_uncultured="${w_d}/bin/scripts/remove_uncultured.py"           # Reduce number of 'uncultured' hits in SILVA (only in highly classified genera)
-export CPU_trim_script="${w_d}/bin/scripts/cpu_dynamic_trim.py"                # Trim exess from SILVA entires (reduce computation time)
+export CPU_trim_script="${w_d}/bin/scripts/cpu_dynamic_trim.py"              # Trim exess from SILVA entires (reduce computation time)
 export iden_high_AQ="${w_d}/bin/scripts/iden_high_AQ.py"                     # Extract high identify (uses % ident rather than AQ algorythm [more sensetive])
 export duplicated_count="${w_d}/bin/scripts/count_duplicate.py"              # Counts duplicated read ids (16s/18s binning QC)
 export bracket_cut="${w_d}/bin/scripts/bracket_cut.py"                       # de-brackets silva entires (standardise formatting)
@@ -54,10 +50,9 @@ export decontaminate="${w_d}/bin/scripts/decontaminate_tsv.py"               # R
 export simplify_taxa="${w_d}/bin/scripts/simplify_taxa.py"                   # Cut kingdom-> <genus species> <- sub...
 export plot_taxa="${w_d}/bin/scripts/plot_taxa.py"                           # Plot genus and species level stacked barchart
 # Text editing ASCII
-export in=$'\e[37m'  # Light gray colour INPUT
-export er=$'\033[0;31m'  # Red colour ERROR
-export su=$'\033[0;32m'  # Green colour SUCCESS
-export r=$'\033[0m'      # Reset colour
-export B=$'\033[1m'      # bold
-
-
+export in=$'\e[37m'              # Light gray colour INPUT
+export er=$'\033[0;31m'          # Red colour ERROR
+export su=$'\033[0;32m'          # Green colour SUCCESS
+export r=$'\033[0m'              # Reset
+export B=$'\033[1m'              # bold
+export or=$'\e[38;2;255;165;0m'  # Orange QUESTION
