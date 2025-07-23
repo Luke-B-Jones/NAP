b22b213d3945f86c50f2afc4005b47d8c074d5d1
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2025-07-23 16:29:50 +0100

    Update simplify_taxa.py

--- scripts/simplify_taxa.py
+++ scripts/simplify_taxa.py
@@ -35,9 +35,10 @@ def process_tsv(input_file, output_file):
         {'taxonomy': 'first', 'abundance': 'sum'}
     )
 
-    # Sort the grouped data by abundance in descending order and save the result
+    # Sort by abundance in descending order
     df_grouped = df_grouped.sort_values(by='abundance', ascending=False)
-   # Output the sorted result as TSV
+
+    # Output the result as TSV
     df_grouped[['taxonomy', 'abundance']].to_csv(output_file, sep='\t', index=False)
 
 # Main function to handle command line arguments
