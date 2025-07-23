ce578276e5befb7a8f7a87950d5c1be47b7ece18
Luke.B.Jones <163171554+Luke-B-Jones@users.noreply.github.com>
2025-07-23 16:10:06 +0100

    Update AMP_template.sh

--- subconfigs/AMP_template.sh
+++ subconfigs/AMP_template.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 # Filtering
-export max_length=""
-export min_length=""
+export max_length=""  # Max length of amplicon expected
+export min_length=""  # Min length of amplicon expected
 # Blastn CON
-export ident_CON_16s="90"
-export ident_CON_18s="90"
+export ident_HAC="" # Identitiy required when alinging RAW to HAC
+export cov_HAC=""   # Coverage required when aligning RAW to HAC
 # Blastn RAW
-export ident_RAW_16s="80"
-export ident_RAW_18s="80"
+export ident_RAW="90" # READ identify to centroid hit
 # Bias correction
-export bias_factor_16s=""
-export bias_factor_18s=""
+export bias_factor_16s="" # abundances bias 16S
+export bias_factor_18s="" # abundances bias 18S
 # Database info
-export for_seq=""
-export rev_seq=""
-export for_range=""
-export rev_range=""
-export ref_min_length_16s=""
-export ref_min_length_18s=""
-export amplicon_16s_length=""
-export amplicon_18s_length=""
-export type=""
+export for_seq="" # sequence of forward
+export rev_seq="" # sequence of reverse
+export for_range="" # into rRNA SSU, base range
+export rev_range="" # into rRNA SSU, base range
+export ref_min_length_16s="" # min length of amplicon
+export ref_min_length_18s="" # min length of amplicon
+export amplicon_16s_length="" # average length of amplicon
+export amplicon_18s_length="" # average length of amplicon
+export type="" # 16s-18s always
